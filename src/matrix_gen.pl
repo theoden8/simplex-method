460e8a5294597599c14b6e117078c69ed75e2016
Kirill Rodriguez <theoden8@gmail.com>
2016-04-26 02:55:01 +0100

    edited bug in matrix_gen.pl : decreased generated matrix by 1 dimension each

--- src/matrix_gen.pl
+++ src/matrix_gen.pl
@@ -12,9 +12,9 @@ my @range = -15..15;
 
 sub num { return $range[rand @range]; }
 
-foreach my $i(0..$h) {
-	foreach my $j(0..$w) {
-        my $num = num();
+foreach my $i(0..($h - 1)) {
+	foreach my $j(0..($w - 1)) {
+		my $num = num();
 		print $num.(" "x(4 - length($num)));
 	}
 	say;
